005894f817f4e815b8179f72662eaeeee95483a5
Carl Räfting <carl@rafting@gmail.com>
2012-04-30 12:25:59 +0200

    A lot of cleanup and some refactoring, sweeeeet!

--- js/designGrid.js
+++ js/designGrid.js
@@ -1,22 +1,22 @@
 /**
  * designGrid.js
- * <url>
- * <url>
+ * http://carlrafting.com/designGrid/
+ * https://github.com/carlrafting/<project>
  */
  
-var designGrid = function () {
+var designGrid = (function () {
 
   function init(options) {
       
     var column_count = (options && options.column_count) || 4,
         grid,
-        divs,
         baseline,
         line_height,
         l,
         columns,
-        len,
-        i;
+        i,
+        output,
+        count;
 
   /**
    * Build html
@@ -24,61 +24,32 @@ var designGrid = function () {
    
     grid = document.createElement('div');
     grid.className = 'grid';
-
-    divs =  ['columns', 'baseline'];
-    len = divs.length;
-    // Build up divs and assign classnames    
-    for (i = 0; i < len; i++) {
-      var classname = divs[i];
-      var d = document.createElement('div');
-      d.className = classname;      
-      divs[i] = d;
-    }
-    divs.forEach(function (el, i, arr) {
-      grid.appendChild(el);
-    });
+    grid.innerHTML += '<div class="columns"></div>' +
+                      '<div class="baseline"></div>';
     document.body.appendChild(grid);
+    // Now that .baseline and .columns are created, we can get them!
+    baseline = document.getElementsByClassName('baseline')[0];
+    columns = document.getElementsByClassName('columns')[0];
   
-    // */
-    
   /**
    * Columns
    */
 
-  baseline = document.getElementsByClassName('baseline')[0];
-  l = document.createElement('div'),
-  columns = document.getElementsByClassName('columns')[0];
+    output = '';
+    for (i = 1; i <= column_count; i+=1) {
+      output += '<div class="col"><small>C' + i + '</small></div>';
+    }
+    columns.innerHTML = output;
    
-   function buildCols() {
-     var output = '';
-     for (i = 1; i <= column_count; i++) {
-       output += '<div class="col"><small>C' + i + '</small></div>';
-     }
-     columns.innerHTML = output;
-   }
-   
-   buildCols();
-   
-   // */
-       
   /**
-   * Baseline stuff
+   * Baseline
    */
    
+    l = document.createElement('div');
     baseline.appendChild(l);
     l.className = 'l';
     
-    // convert string into number
-    // NOT USED, REMOVE!
-
-    function convertToNum(value) {
-      value = value.slice(0, -2); // Strip off px from string.
-      value = Number(value);
-      return value;
-    }
-    
     // now it's time to convert the value of line_height to a number
-    // line_height = convertToNum(line_height);
     line_height = document.getElementsByClassName('l')[0];
     line_height = line_height.offsetHeight;
     
@@ -88,29 +59,15 @@ var designGrid = function () {
       return result;
     }
     
-    // get viewport hight and store it as count
-    var count = x();
-    // we need to get the new value on resize as well... 
-    // not currently of any use though... But maybe later on
-    window.addEventListener('resize', function (event) {
-      count = x();
-      // Wow this has a huge performance hit! Maybe remove or optimize somehow?
-      // Oh and btw, i wrapped buildLines() in setTimeout to reduce the performance hit, didn't do much. 
-      // The problem must be somewhere else, the loop most likely... window resize and loops, not a good combination i guess...
-      // setTimeout(buildLines, 3000);
-    }, false);
+    // get viewport height and store it as count
+    count = x();
     
     // build up html for baseline
-    function buildLines() {
-      var output = "";
-      for (i = 0; i < count; i++) {
-        output += '<div class="l"></div>';
-      }
-      baseline.innerHTML = output;
+    output = '';
+    for (i = 0; i < count; i+=1) {
+      output += '<div class="l"></div>';
     }
-    
-    buildLines();
-    // */
+    baseline.innerHTML = output;
     
   /**
    * Hide/Show grid overlay
@@ -126,34 +83,35 @@ var designGrid = function () {
         } else {
           grid.classList.remove('hide');
         }
-        storestate();
+        designGrid.storestate(grid);
       }
     }, false);
-    // */
-    
-  /**
-   * State functionality
-   */
-    
-    // store the className of grid
-    function storestate() {
-      localStorage.grid = grid.className;
-    }
-    
-    // retrieve whatever is stored
-    function retrievestate() {
-      if (localStorage.grid) {
-        grid.className = localStorage.grid;
-      }
-    }
     
     // retrieve state on DOMContentLoaded (similar to jQuery's $(document).ready())
-    document.addEventListener('DOMContentLoaded', retrievestate, false);  
+    document.addEventListener('DOMContentLoaded', designGrid.retrievestate(grid), false);  
       
-    // */
-      
-  };
+  }
   
-  return { init: init };
+/**
+ * State functionality
+ */
+  
+  // store the className of grid
+  function storestate(el) {
+    localStorage.designGrid = el.className;
+  }
+  
+  // retrieve whatever is stored
+  function retrievestate(el) {
+    if (localStorage.designGrid) {
+      el.className = localStorage.designGrid;
+    }
+  }
+  
+  return {
+    init: init,
+    retrievestate: retrievestate,
+    storestate: storestate
+  };
 
-}();
+}());
